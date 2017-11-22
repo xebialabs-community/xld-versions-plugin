43f6a0a8c81a4da4fba7ad106ba83d2bce93684b
Joris De Winne <joris.dewinne@gmail.com>
2017-11-22 11:34:36 -0800

    Update license and gradle wrapper

--- src/main/resources/web/xld-versions-plugin/bootstrap-3.2.0/js/bootstrap.js
+++ src/main/resources/web/xld-versions-plugin/bootstrap-3.2.0/js/bootstrap.js
@@ -1,8 +1,3 @@
-/*
- * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
- * FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
- */
 if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript requires jQuery') }
 
 /* ========================================================================
